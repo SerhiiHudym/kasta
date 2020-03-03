d750874f42d11294e5c628dcc6c932130d3ac8e1
Serhii Hudym <serhiihudym@gmail.com>
2020-03-03 18:34:00 +0200

    Updates

---
+++ precache-manifest.a72dc38bcd5475e20314653c21e03673.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df84e002d856540d70622c57e43f065",
+    "url": "/kasta/index.html"
+  },
+  {
+    "revision": "10936defccc4b6b5e14f",
+    "url": "/kasta/static/css/main.03d552fe.chunk.css"
+  },
+  {
+    "revision": "0c476babe8f6a3a3088f",
+    "url": "/kasta/static/js/2.cb2351a2.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/kasta/static/js/2.cb2351a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10936defccc4b6b5e14f",
+    "url": "/kasta/static/js/main.5c89f573.chunk.js"
+  },
+  {
+    "revision": "1d747ae6fde576465327",
+    "url": "/kasta/static/js/runtime-main.19d3664a.js"
+  },
+  {
+    "revision": "b06d686ce26a343b0210c0734a3e476b",
+    "url": "/kasta/static/media/check-green.b06d686c.png"
+  },
+  {
+    "revision": "6f784d942f1e3de42b2a7f7bd812c87b",
+    "url": "/kasta/static/media/kasta-logo.6f784d94.png"
+  },
+  {
+    "revision": "b3c8b159b5fb60e600287bb6ae51f633",
+    "url": "/kasta/static/media/mc.b3c8b159.png"
+  },
+  {
+    "revision": "777fad0e7219f44905cd4e7dcb79df88",
+    "url": "/kasta/static/media/pci.777fad0e.png"
+  },
+  {
+    "revision": "76933251088f088828653c8f1c160ffd",
+    "url": "/kasta/static/media/visa.76933251.png"
+  }
+]);
